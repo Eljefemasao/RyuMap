96c17e525b171ff084aee8e021d4b0ba341b8e16
Masaaki Matsunaga <castle2164m@gmail.com>
2018-03-17 21:46:23 +0900

    logout page

---
+++ html/Logout.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+
+if (isset($_SESSION["NAME"])) {
+    $errorMessage = "ログアウトしました。";
+} else {
+    $errorMessage = "セッションがタイムアウトしました。";
+}
+
+// セッションの変数のクリア
+$_SESSION = array();
+
+// セッションクリア
+@session_destroy();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../assets/css/jumbotron.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+          </button>
+            <a class="navbar-brand" href="javascript:location.reload();"> RyuMap</a>
+          </div>
+          </form>
+
+      </div>
+    </nav>
+    <body>
+      <div class="jumbotron">
+        <div class="container">
+        <h2>Logout Form</h2>
+        <div><?php echo htmlspecialchars($errorMessage, ENT_QUOTES); ?></div>
+        <ul>
+            <li><a href="Login.php">go back login form</a></li>
+        </ul>
+      </div>
+    </div>
+    </body>
+</html>
